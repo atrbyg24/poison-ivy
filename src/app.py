4595836450b2f72f5017f910d3825d6b9d642ef5
atrbyg24 <artwan520@gmail.com>
2025-09-16 16:15:00 -0400

    Train different models and populate dashboard

--- src/app.py
+++ src/app.py
@@ -25,18 +25,20 @@ runs_df = mlflow.search_runs([exp_id], order_by=["attributes.start_time DESC"], 
 if runs_df.empty:
     st.warning("No runs found")
     st.stop()
-st.dataframe(runs_df[["run_id","metrics.val_accuracy","params.model","params.epochs"]])
+st.dataframe(runs_df[["run_id","metrics.val_accuracy","params.model","params.epochs", "params.lr"]])
 
 selected = st.multiselect("Select run(s) to compare", runs_df["run_id"].tolist(), max_selections=3)
+
 def to_df(history):
     return pd.DataFrame([(m.step,m.value) for m in history], columns=["step","value"])
+
 if selected:
     fig, axs = plt.subplots(1,2,figsize=(12,4))
     for r in selected:
         for metric, ax in [("train_loss", axs[0]), ("val_loss", axs[0]), ("train_accuracy", axs[1]), ("val_accuracy", axs[1])]:
             hist = client.get_metric_history(r, metric)
             if hist:
-                df = to_df(hist)
+                df = to_df(hist)[: -1]
                 linestyle = "-" if "train" in metric else "--"
                 ax.plot(df["step"], df["value"], linestyle=linestyle, label=f"{r[:6]}-{metric}")
     axs[0].set_title("Loss"); axs[0].legend()
@@ -61,12 +63,15 @@ if uploaded:
                            T.ToTensor(),
                            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                            ])
-    arr = transform(img).unsqueeze(0)   
+    arr = transform(img).unsqueeze(0)
+
     CLASS_NAMES = {0: 'Nontoxic', 1: 'Toxic'}
+    
     with torch.no_grad():
         out = model(arr)
         probs = torch.softmax(out, dim=1).numpy()[0]
         pred_class_idx = int(np.argmax(probs))
         pred_class_name = CLASS_NAMES.get(pred_class_idx, "Unknown")
         pred_class_prob = probs[pred_class_idx]
+    
     st.write(f"The plant is most likely {pred_class_name} with probability {pred_class_prob: .4f}")
